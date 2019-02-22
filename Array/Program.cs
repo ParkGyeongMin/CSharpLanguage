73f8a2fbe4b942482539b26fa177f9ee9e8dc353
ParkGyeongMin <dlfjsskawk00@naver.com>
2019-02-22 18:01:42 +0900

    add Array

---
+++ Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //배열 초기화 하는 방법 
+            string[] array1 = new string[3] {"안녕", "Hello", "Hi"};
+            string[] array2 = new string[] {"hello", "안녕", "Hi"};
+            string[] array3 = {"hi", "hi", "hello"};
+
+            Console.WriteLine("Array1 ....");
+            foreach (var st in array1)
+            {
+                Console.WriteLine("{0}",st);
+            }
+            Console.WriteLine("Array2 ....");
+            foreach (var st in array2)
+            {
+                Console.WriteLine("{0}",st);
+            }
+            Console.WriteLine("Array3 ....");
+            foreach (var st in array3)
+            {
+                Console.WriteLine("{0}",st);
+            }
+            
+        }
+    }
+}
